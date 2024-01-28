4d672c33a5e6a52dd89ab7b000183a48deff4a99
Ashistorq <ashishsinghtarwa@gmail.com>
2024-01-29 02:51:58 +0530

    Add header component with logo and navigation

--- 01 React/01 Puma/Puma/src/Components/Header/Header.jsx
+++ 01 React/01 Puma/Puma/src/Components/Header/Header.jsx
@@ -1,11 +1,31 @@
-import React, { Fragment } from 'react'
-import "./Header.css"
+import React, { Fragment } from "react";
+import "./Header.css";
 const Header = () => {
   return (
     <Fragment>
-      
+      <header className="header-containter">
+        <div className="logo">
+          <img
+            src="https://assets.turbologo.com/blog/en/2019/11/19084917/puma-logo-cover-958x575.png"
+            alt="Puma logo"
+          />
+        </div>
+        <div className="head-content">
+          <ul className="list-item">
+            <input
+              type="text"
+              className="search_input"
+              placeholder="Search for goods"
+            />
+            <li>Home</li>
+            <li>About</li>
+            <li>Contack</li>
+            <li>Outlet</li>
+          </ul>
+        </div>
+      </header>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
