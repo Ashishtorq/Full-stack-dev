4efa9c42086162a3faf2a7a6642cbff91acacaff
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-11 01:35:47 +0530

    Add external CSS library and update header and product components

--- 04 E-comm/src/Components/Header/Header.jsx
+++ 04 E-comm/src/Components/Header/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import {React, Fragment} from 'react'
 import "./Header.css"
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Fragment>
+      <div id="head">
+        <h6>Home</h6>
+        <h6>
+          Cart <i class="ri-shopping-cart-2-line"></i>
+        </h6>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Header
--- 04 E-comm/src/Components/Product/Product.jsx
+++ 04 E-comm/src/Components/Product/Product.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import { Fragment } from "react";
+import Data from "../Data.json";
+import { useState } from "react";
 const Product = () => {
+  const [Products, setProducts] = useState([]);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Fragment>
+      <div id="container">
+        {Data.map((data) => {
+          return (
+            <div id="card" key={data.id}>
+              <img src={data.thumbnail} alt="thumbnail" />
+              <p>{data.title}</p>
+              <p>{data.price}</p>
+              <button>ADD</button>
+            </div>
+          );
+        })}
+      </div>
+    </Fragment>
+  );
+};
 
-export default Product
+export default Product;
