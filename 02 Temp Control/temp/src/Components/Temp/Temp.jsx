908db92f277421b4557a69ac4083859c254a86b5
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-10 13:09:50 +0530

    Update button color based on temperature

--- 02 Temp Control/temp/src/Components/Temp/Temp.jsx
+++ 02 Temp Control/temp/src/Components/Temp/Temp.jsx
@@ -3,18 +3,27 @@ import "./Temp.css";
 const Temp = () => {
   const [temp, setTemp] = useState(5);
   const [color, setColor] = useState("#33495D");
+  const [btncolor, setbtncolor] = useState("blanchedalmond");
   
   const plus = () => {
     setTemp(temp + 1);
     if(temp >= 21){
         setColor("orangered")
     }
+    if(temp >= 21){
+        setbtncolor("orangered");
+    }
+   
+
    
   };
   const minus = () => {
     setTemp(temp - 1);
     if(temp <= 22){
         setColor("#33495D");
+    }
+    if(temp <= 22){
+        setbtncolor("#33495D");
     }
   };
   return (
@@ -26,10 +35,10 @@ const Temp = () => {
           </h6>
         </div>
         <div id="btn">
-          <button onClick={plus} style={{ marginRight: "10px" }}>
+          <button onClick={plus} style={{ marginRight: "10px", backgroundColor:btncolor }}>
             <i class="ri-add-box-line"></i>
           </button>
-          <button onClick={minus}>
+          <button onClick={minus} style={{backgroundColor:btncolor}}>
             <i class="ri-subtract-fill"></i>
           </button>
         </div>
