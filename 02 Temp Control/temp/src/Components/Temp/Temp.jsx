6dc439d8a9f1c0ae18467525f1e14f825ee9ae02
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-10 13:01:30 +0530

    Update Temp component styling and logic

--- 02 Temp Control/temp/src/Components/Temp/Temp.jsx
+++ 02 Temp Control/temp/src/Components/Temp/Temp.jsx
@@ -2,17 +2,28 @@ import React, { Fragment, useState } from "react";
 import "./Temp.css";
 const Temp = () => {
   const [temp, setTemp] = useState(5);
+  const [color, setColor] = useState("#33495D");
+  
   const plus = () => {
     setTemp(temp + 1);
+    if(temp >= 21){
+        setColor("orangered")
+    }
+   
   };
   const minus = () => {
     setTemp(temp - 1);
+    if(temp <= 22){
+        setColor("#33495D");
+    }
   };
   return (
     <Fragment>
       <div id="containter">
-        <div id="screen">
-          <h6>{temp}</h6>
+        <div id="screen" style={{backgroundColor:color}}>
+          <h6 style={{display:"flex", alignItems:"center",justifyContent:"center"}}>
+            {temp} <span>°C</span>
+          </h6>
         </div>
         <div id="btn">
           <button onClick={plus} style={{ marginRight: "10px" }}>
