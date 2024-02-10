bdc969a2a38dcb1ee4a5ebca1042e9d4e7c2f25b
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-10 11:38:11 +0530

    Add temperature control buttons

--- 02 Temp Control/temp/src/Components/Temp/Temp.jsx
+++ 02 Temp Control/temp/src/Components/Temp/Temp.jsx
@@ -1,16 +1,30 @@
-import React, { Fragment, useState } from 'react'
-import "./Temp.css"
+import React, { Fragment, useState } from "react";
+import "./Temp.css";
 const Temp = () => {
-    const [temp, setTemp] = useState(5)
+  const [temp, setTemp] = useState(5);
+  const plus = () => {
+    setTemp(temp + 1);
+  };
+  const minus = () => {
+    setTemp(temp - 1);
+  };
   return (
     <Fragment>
       <div id="containter">
         <div id="screen">
-       <h6>{temp}</h6>
-        </div>      
+          <h6>{temp}</h6>
+        </div>
+        <div id="btn">
+          <button onClick={plus} style={{ marginRight: "10px" }}>
+            <i class="ri-add-box-line"></i>
+          </button>
+          <button onClick={minus}>
+            <i class="ri-subtract-fill"></i>
+          </button>
+        </div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Temp
+export default Temp;
