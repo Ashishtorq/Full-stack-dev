991e298da4f9d2a653481ac6a5554c2e1a460592
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-11 00:17:28 +0530

    Fix bug in login functionality

---
+++ 04 E-comm/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Fragment } from 'react'
+import Header from './Components/Header/Header'
+import Product from './Components/Product/Product'
+function App() {
+  
+
+  return (
+    <Fragment>
+     <Header></Header>
+     <Product></Product>
+    </Fragment>
+  )
+}
+
+export default App
