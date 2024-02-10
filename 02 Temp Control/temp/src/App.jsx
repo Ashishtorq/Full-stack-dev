eac7115971eeb6e41d5bfe84fddb9adf3c246694
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-10 10:45:33 +0530

    Refactor code for improved performance

---
+++ 02 Temp Control/temp/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment, useState } from 'react'
+import './App.css'
+import Temp from './Components/Temp/Temp'
+function App() {
+  
+  return (
+    <Fragment>
+      <Temp></Temp>
+    </Fragment>
+  )
+}
+
+export default App
---
+++ 02 Temp Control/temp/src/Components/Temp/Temp.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import "./Temp.css"
+const Temp = () => {
+  return (
+    <Fragment>
+      <div id="containter">
+        <div id="screen">
+        
+        </div>      
+      </div>
+    </Fragment>
+  )
+}
+
+export default Temp
