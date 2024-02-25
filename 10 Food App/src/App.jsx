2c9b24c0217db176706fc9ba36b91d65c828c3c3
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-25 15:37:07 +0530

    Refactor code to improve performance and readability

---
+++ 10 Food App/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, Fragment } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Fragment>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Fragment>
+  );
+}
+
+export default App
