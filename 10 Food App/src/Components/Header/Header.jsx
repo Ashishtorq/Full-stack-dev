845557133b3ac164cee277185c2101044b71cc23
Ashistorq <ashishsinghtarwa@gmail.com>
2024-02-26 10:18:23 +0530

    Update Header component styles

--- 10 Food App/src/Components/Header/Header.jsx
+++ 10 Food App/src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { FaCartShopping } from "react-icons/fa6";
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import { RxCross1 } from "react-icons/rx";
 import "./Header.css";
 const Header = () => {
   return (
@@ -29,5 +29,18 @@ const Header = () => {
     </Fragment>
   );
 };
-
+  
 export default Header;
+
+
+
+
+ {/*<div id="navigations">
+   <RxCross1 id="cross" />
+   <ul>
+     <li>Home</li>
+     <li>About Us</li>
+     <li>Offers</li>
+     <li>Sign Up</li>
+   </ul>
+</div>;*/}
---
+++ 10 Food App/src/Components/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Hero = () => {
+  return (
+    <Fragment>
+      
+    </Fragment>
+  )
+}
+
+export default Hero
